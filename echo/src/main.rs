23ac77d801be2cecf9de1f18df0057eb943dc7e3
haumy143 <66621176+haumy143@users.noreply.github.com>
2023-10-18 20:39:50 +0200

    update main.rs

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let arg1 = std::env::args().nth(1).expect("no arguments");
+    if arg1.starts_with("$"){
+        if let Some(var_name) = arg1.strip_prefix("$"){
+            match env::var(var_name) {
+                Ok(val) => {
+                    println!("{}", val);
+                }
+                Err(_) => {
+                    println!("Environment variable {} not found", var_name)
+                }
+        }
+    }
+}
+    println!("{}", arg1);
 }
